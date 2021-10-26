9c3e96faa7a477fdfb98a547e7c8c3b5d5c398d7
TranNangDo <trannangdo@gmail.com>
2021-10-26 16:22:55 +0700

    Scroll

--- js/main.js
+++ js/main.js
@@ -65,8 +65,31 @@ Address : ${address}`
     document.getElementById("warning-address").innerHTML = textAddress;
 }
 
+
 setTimeout(function() {
     let x = document.querySelector(".load")
         // console.log(x);
     x.style.display = "none";
 }, 3000);
+
+
+
+
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
